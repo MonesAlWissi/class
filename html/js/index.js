33454d7d0e89c015d4afa03bf426205e40117e74
MonesAlWissi <alwissi.mones@gmail.com>
2024-11-23 16:12:49 +0300

    t

---
+++ html/js/index.js
@@ -0,0 +1,250 @@
+document.getElementById('mones')
+var titel1=document.getElementById('mones')
+titel1.innerHTML="mmmmmmmmmmm"
+titel1.style.color='red'
+titel1.style.backgroundColor='blue'
+const name1="kaled"
+newDiv.innerHTML=`<h1>mones mohamed alwissi<h1> ${name1} `
+var newClass=document.getElementsByClassName('newTitle')
+newClass[0].innerHTML='abeeer aldhon'
+var findP=document.getElementsByTagName('p')
+console.log(findP);
+
+// findP[4].innerHTML="yazan numat"
+// for(let i=0 ;i<findP.length;i++){
+//     findP[i].innerHTML="yaza"
+//     if(i%2==0){
+//         findP[i].style.color="red"
+//     }
+//     else{
+//         findP[i].style.color="orange"
+//     }
+ 
+// }
+// var newlist1 = document.getElementsByClassName('newlist')[0]
+// var liJS =document.createElement('li')
+// var h3JS = document.createElement('h3')
+// h3JS.innerHTML="i am h3 from js"
+// var pJS=document.createElement('p')
+// pJS.innerHTML="i am p from js"
+// liJS.appendChild(h3JS)
+// liJS.appendChild(pJS)
+// newlist1.appendChild(liJS)
+// i am h3 from js
+// i am p from js
+
+
+
+// var newlist1=document.getElementsByClassName('newlist')[0]
+// var liJS=document.createElement('li')
+// var h3JS =document.createElement('h3')
+// h3JS.innerHTML="i am h3 from js"
+// var pJS=document.createElement('p')
+// pJS.innerHTML="i am p from js"
+
+// liJS.appendChild(h3JS)
+// liJS.appendChild(pJS)
+// newlist1.appendChild(liJS)
+
+
+
+
+
+
+
+var newTitle1 = document.getElementsByClassName('newlist')[0]
+var liJS = document.createElement("li")
+var h3Js = document.createElement('h3')
+ h3Js.innerHTML="mones mohamed h3"
+var  pJS= document.createElement('p')
+pJS.innerHTML="mones mohamed alwissi p"
+liJS.appendChild(h3Js)
+liJS.appendChild(pJS)
+newTitle1.appendChild(liJS)
+
+
+// function sumation(){
+//   var input1 =document.getElementById('input1').value
+//   var input2 =document.getElementById('input2').value
+//   var z= parseInt(input1)+parseInt(input2)
+//   var rusult =document.getElementById('result')
+//   rusult.innerHTML=z
+// }
+// }
+// function sumation (){
+//   var input11=document.getElementById('input11').value
+//   var input22=document.getElementById('input22').value
+//   var z = parseInt(input11)+parseInt(input22)
+//   var result = document.getElementById('result')
+
+//   result.innerHTML=z
+  
+// }
+
+
+
+
+// function Subtract (){
+// var number1=document.getElementById('nimber1').value
+// var number2=document.getElementById('nimber2').value
+// var z= parseInt(number1)-parseInt(number2)
+// var result1=document.getElementById('result1')
+// result1.innerHTML=z
+
+// }
+
+
+
+
+// function Mult (){
+//   var num1=document.getElementById('num1').value
+//   var num2=document.getElementById('num2').value
+//   var z= parseInt(num1)/parent(num2)
+//   var result2=document.getElementById('result2')
+//   result2.innerHTML=z
+
+// }
+
+// function operation(){
+//   var input1 =document.getElementById('input1').value
+//   var input2 =document.getElementById('input2').value
+  
+//   var rusult =document.getElementById('result')
+//   rusult.innerHTML=z
+// }
+
+
+
+// switch (operation){
+//   case "+":
+//     var z= parseInt(input1)+parent(input2)
+//    break;
+   
+
+
+// function sumation(){
+// var input1=document.getElementById('input1').value
+// var input2=document.getElementById('input1').value
+// var z = parseInt(input1)+ parseInt(input2)
+// var ruselt =document.getElementById('ruselt')
+
+// ruselt.innerHTML=z;
+// }
+
+function mones ()  {
+  var number1 = document.getElementById('number1').value
+  var number2 = document.getElementById('number2').value
+  var z = parseInt(number1)+parseInt(number2)
+  var result=document.getElementById('result')
+  result.innerHTML=z
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
